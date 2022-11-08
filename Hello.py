75ba970bd1638ef9a2bd19220a88c6ee86faa2fa
mateusz dalba <mateusz.dalba96@gmail.com>
2022-11-08 17:01:40 +0100

    M

--- Hello.py
+++ Hello.py
@@ -1,4 +1,8 @@
 import streamlit as st
+import sys
+from streamlit.web import cli as stcli
+from streamlit import runtime
+import os
 
 
 st.set_page_config(page_title='Grind',
@@ -10,3 +14,9 @@ st.set_page_config(page_title='Grind',
 st.title("Grind")
 
 st.sidebar.header('Virtual Gym')
+
+
+
+#if __name__ == '__main__':
+#    os.system('streamlit run Hello.py')#The file to start the crawler project
+
